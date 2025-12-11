1e2632f0437c8552578cfb10250dd3973cfaece0
Rodrigo Cardoso <erinofgok@gmail.com>
2025-12-11 12:37:20 -0300

    Adjust button positioning in VideosTab component

--- src/components/VideosTab.tsx
+++ src/components/VideosTab.tsx
@@ -62,8 +62,8 @@ const VideosTab = () => {
         onClick={scrollToBottom}
         className="
           fixed
+          bottom-6
           left-6
-          right-6
           bg-pink-500
           text-white
           p-4
