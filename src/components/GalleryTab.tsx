0c7f6141aa2af247ecbb6b1d81e32a87b8f84b61
Rodrigo Cardoso <erinofgok@gmail.com>
2025-12-24 18:37:52 -0300

    Change img8 import from PNG to JPEG format

--- src/components/GalleryTab.tsx
+++ src/components/GalleryTab.tsx
@@ -10,7 +10,7 @@ import img2 from '@/assets/2.png';
 import img3 from '@/assets/3.png';
 import img4 from '@/assets/4.png';
 import img7 from '@/assets/7.png';
-import img8 from  '@/assets/8.png';
+import img8 from  '@/assets/8.jpeg';
 import img9 from '@/assets/9.jpeg';
 import img10 from '@/assets/10.jpeg';
 import img11 from '@/assets/11.jpeg';
